cc9d8e11169ee0c26e326f626b2758ca46f3c076
Jake Jongejans <40899982+Javernus@users.noreply.github.com>
2025-04-07 14:04:46 +0000

    fix: Rewrote broken code

--- fabric/node_scripts/start_kubespray.sh
+++ fabric/node_scripts/start_kubespray.sh
@@ -4,5 +4,9 @@
 cd kubespray
 source ./venv/bin/activate
 
+mkdir -p $HOME/.kube
+sudo cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
 ansible-playbook -i inventory/dynamos/inventory.ini cluster.yml -b -v -u ubuntu --private-key=~/.ssh/slice_key
 }
