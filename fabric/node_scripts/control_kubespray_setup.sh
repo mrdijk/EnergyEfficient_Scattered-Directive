2c82e805f551590f1824220d4b7b0c578ffd5389
Jake Jongejans <40899982+Javernus@users.noreply.github.com>
2025-04-07 13:59:37 +0200

    chore: Rename kubespray inventory folder to dynamos.

--- fabric/node_scripts/control_kubespray_setup.sh
+++ fabric/node_scripts/control_kubespray_setup.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-subnet=$1
-ip=$2
-
 {
 git clone https://github.com/kubernetes-sigs/kubespray.git
 cd kubespray
 git checkout release-2.27
 pip3 install -r requirements.txt
 
-mv inventory/sample inventory/dynamos-on-fabric
+mv inventory/sample inventory/dynamos
 
 ansible version
 }  2>&1 | tee -a start_control_plane.log
