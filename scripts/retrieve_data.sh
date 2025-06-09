4de323a5ed9df90f22bb5f26ba44208b5425ed66
Jake Jongejans <40899982+Javernus@users.noreply.github.com>
2025-06-09 10:32:33 +0200

    chore: Fix retrieval again.

--- scripts/retrieve_data.sh
+++ scripts/retrieve_data.sh
@@ -3,7 +3,7 @@
 API_GATEWAY_POD=$(kubectl get pods -n api-gateway | grep api-gateway | sed "s/^\(api-gateway[a-zA-Z0-9-]\+\).*/\1/")
 logs=$(kubectl logs $API_GATEWAY_POD -c api-gateway -n api-gateway | sed "s/\t/ /")
 
-read -ra intermediate_accuracies <<< "$(echo "$logs" | awk '
+intermediate_accuracies=$(echo "$logs" | awk '
   /Intermediate accuracy achieved:/ {
     for (i = 1; i <= NF; ++i) {
       if ($i ~ /^[0-9]+(\.[0-9]+)?$/) {
@@ -11,7 +11,7 @@ read -ra intermediate_accuracies <<< "$(echo "$logs" | awk '
         break
       }
     }
-  }')"
+  }')
 
 final_accuracy=$(echo "$logs" | awk '
   /Final accuracy achieved:/ {
